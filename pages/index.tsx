64ff8772cda29d6501ff11c385fc051ca26a6d4e
Gabriel <contato@codie.com>
2024-04-23 11:21:40 -0300

    att

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,12 @@
-import { Input, InputFile, api, useSocialLinks } from "codiedigital";
 import { Layout } from "components/layout";
-import { FormHandler } from "src/utils/form-handler";
 
 export default function HomePage(props) {
-  const { telefone } = useSocialLinks();
+  props.data;
 
-  props.data
-
-  return (
-    <Layout>
-
-      {props.data?.banner && <Banner banner={props.data.banner} />}
-
-  
-    </Layout>
-  );
+  return <Layout></Layout>;
 }
 
 export const getServerSideProps = async (ctx) => {
-  const response = await api.get("home");
-
   return {
     props: {
       data: response.data,
