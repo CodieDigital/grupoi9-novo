177a2f550736e415fe0b01900a9013c095a1a7c1
CodieDigital <50671603+CodieDigital@users.noreply.github.com>
2024-06-11 12:00:19 -0300

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -34,7 +34,32 @@ export default class MyDocument extends Document {
     render() {
         return (
             <Html lang="en">
-                <Head></Head>
+                <Head>
+                 <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function (w, d, s, o, f, js, fjs) {
+                  w[o] = w[o] || function () { (w[o].q = w[o].q || []).push(arguments) };
+                  js = d.createElement(s), fjs = d.getElementsByTagName(s)[0];
+                  js.id = o; js.src = f; js.async = 1;
+                  fjs.parentNode.insertBefore(js, fjs);
+                }(window, document, 'script', 'widget4Events', 'https://cdn.4.events/mi-widget/bundle.js'));
+                widget4Events('init', {
+                  client_id: '24653',
+                  event_id: '1',
+                  position: 'left',
+                  button_text: 'GARANTA SUA VAGA',
+                  language: 'pt-br',
+                  image: 'https://vp2uploads.s3.amazonaws.com/24653/1/imagem_fundo_widget/7d94b781f07ad4a2cea391286ac3277e156c51b2.jpg',
+                  theme: {
+                    button_color: '#00b4af',
+                    button_text_color: '#ffffff',
+                  },
+                });
+              `,
+            }}
+          />
+                </Head>
 
                 <body >
                     <Main/>
